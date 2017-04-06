9616fb6f9a823f4798cd71242745ef51e9148150
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2017-04-06 18:07:47 -0500

    Se crearon el resto de los modelos de la BD

---
+++ app/Models/Apuesta.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Apuesta extends Model
+{
+    protected $table = 'apuesta';
+    public $timestamps = false;
+}
---
+++ app/Models/ApuestaPartido.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ApuestaPartido extends Model
+{
+    protected $table = 'apuesta_partido';
+    public $timestamps = false;
+}
