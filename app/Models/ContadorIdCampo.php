deb7caac3c14b58e2c286d065ee06a590bf42672
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2017-06-13 10:57:22 -0500

    Se me olvidó agregar el modelo ContadorIdCampo

---
+++ app/Models/ContadorIdCampo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ContadorIdCampo extends Model
+{
+    protected $table = 'contador_id_campo';
+    public $timestamps = false;
+}
